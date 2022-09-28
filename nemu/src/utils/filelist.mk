9defe80633b69418f919edecf65bffef31560321
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 09:26:58 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:26:58 up 52 min,  1 user,  load average: 0.57, 0.47, 0.54

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -15,6 +15,6 @@
 
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
+CXXFLAGS += $(shell llvm-config-13 --cxxflags) -fPIE
 LIBS += $(shell llvm-config --libs)
 endif
