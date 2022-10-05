40bdac688dac6d86d013387ae4dfffd6306429a6
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-05 21:15:39 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:15:39 up 12:51,  1 user,  load average: 0.20, 0.29, 0.27

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -108,7 +108,7 @@ static int cmd_x(char *args) {
   raddr=maddr;
   for(int i=0;i<n;i++){
   	printf("0x%lx\t",raddr);
-  	printf("%c\n",*(unsigned char *)raddr);
+  	printf("%d\n",*(unsigned int *)raddr);
   	raddr += 4;
   }
   
