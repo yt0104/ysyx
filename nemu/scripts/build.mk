68b1411a6cd081a4a079c38b4a19edefd4ad88dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-05 10:40:44 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:40:43 up  2:16,  1 user,  load average: 0.46, 0.44, 0.38

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -29,17 +29,34 @@ OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
 # Compilation patterns
 $(OBJ_DIR)/%.o: %.c
-	@echo + CC $<
-	@mkdir -p $(dir $@)
+	@echo + CC $<				
+	@mkdir -p $(dir $@)			
 	@$(CC) $(CFLAGS) -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
+	
+	#1
+	#echo + CC src/nemu-main.c
+	#mkdir -p /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/
+	#gcc -O2 -MMD -Wall -Werror -I/home/crofy/ysyx-workbench/nemu/include -I/home/crofy/ysyx-workbench/nemu/src/isa/riscv64/include -I/home/crofy/ysyx-workbench/nemu/src/engine/interpreter -O2  -Og -ggdb3  -DITRACE_COND=true -D__GUEST_ISA__=riscv64 -c -o /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/nemu-main.o src/nemu-main.c
+	#/home/crofy/ysyx-workbench/nemu/tools/fixdep/build/fixdep  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/nemu-main.d  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/nemu-main.o unused >  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/nemu-main.d.tmp
+	#mv  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/nemu-main.d.tmp  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/nemu-main.d
 
 $(OBJ_DIR)/%.o: %.cc
 	@echo + CXX $<
 	@mkdir -p $(dir $@)
 	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
-
+	
+	#2
+	#echo + CXX src/utils/disasm.cc
+	#mkdir -p /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/utils/
+	#g++ -O2 -MMD -Wall -Werror -I/home/crofy/ysyx-workbench/nemu/include -I/home/crofy/ysyx-workbench/nemu/src/isa/riscv64/include -I/home/crofy/ysyx-workbench/nemu/src/engine/interpreter -O2  -Og -ggdb3  -DITRACE_COND=true -D__GUEST_ISA__=riscv64 -I/usr/lib/llvm-13/include -std=c++14   -fno-exceptions -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -fPIE -c -o /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/utils/disasm.o src/utils/disasm.cc
+	#/home/crofy/ysyx-workbench/nemu/tools/fixdep/build/fixdep  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/utils/disasm.d  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/utils/disasm.o unused >  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/utils/disasm.d.tmp
+	#mv  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/utils/disasm.d.tmp  /home/crofy/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/utils/disasm.d
+	#flock /home/crofy/ysyx-workbench/nemu/../.git/ make -C /home/crofy/ysyx-workbench/nemu/.. .git_commit MSG=' "compile NEMU"'
+	#sync
+	
+	
 # Depencies
 -include $(OBJS:.o=.d)
 
