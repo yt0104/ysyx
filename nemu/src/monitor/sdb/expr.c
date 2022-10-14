6fd1bdcd81ac5f56bbbc403ea961d77b18eda965
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-14 16:25:55 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:25:55 up  1:16,  1 user,  load average: 0.17, 0.18, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -117,6 +117,7 @@ static bool make_token(char *e) {
           case '*':	tokens[nr_token++].type=rules[i].token_type;	break;
           case '/':	tokens[nr_token++].type=rules[i].token_type;	break;
           case TK_NUM:	strncpy(tokens[nr_token].str,substr_start,substr_len); 
+          		tokens[nr_token].str[substr_len] = '\0';
           		tokens[nr_token++].type =rules[i].token_type;
           		break;
           case '(':	tokens[nr_token++].type=rules[i].token_type;	break;
@@ -125,9 +126,11 @@ static bool make_token(char *e) {
           case TK_UEQ:	tokens[nr_token++].type=rules[i].token_type;	break; 
           case TK_AND:	tokens[nr_token++].type=rules[i].token_type;	break;
           case TK_HEX:	strncpy(tokens[nr_token].str,substr_start+2,substr_len-2); 
+          		tokens[nr_token].str[substr_len-2] = '\0';
           		tokens[nr_token++].type =rules[i].token_type;
           		break;  
           case TK_REG:	strncpy(tokens[nr_token].str,substr_start+1,substr_len-1); 
+          		tokens[nr_token].str[substr_len-1] = '\0';
           		tokens[nr_token++].type =rules[i].token_type;	
           		break;  
           default: TODO();
