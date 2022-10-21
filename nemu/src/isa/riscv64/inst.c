cec4a4bed6e0cbfe0b37d2bd9ef010bed93cd1b3
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-21 20:45:18 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:45:18 up  6:01,  1 user,  load average: 0.53, 0.34, 0.33

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -40,7 +40,8 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
   int rs2 = BITS(i, 24, 20);	//---SEXT:符号扩展
   *dest = rd;
   switch (type) {
-    case TYPE_I: src1R();          immI(); break;
+    case TYPE_I: src1R();         
+     immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
   }
