1b75b242aeac95c0c7023f00f2393666679ac7e8
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 16:33:33 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:33 up  7:34,  1 user,  load average: 1.37, 1.29, 0.98

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,TYPE_J,TYPE_R,
+  TYPE_I, TYPE_U, TYPE_S,TYPE_J,TYPE_R,TYPE_B,
   TYPE_N, // none
 };
 
@@ -34,7 +34,8 @@ enum {
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (SEXT(BITS(i, 19, 12), 8) << 12) \
   | (SEXT(BITS(i, 20, 20), 1) << 11) | SEXT(BITS(i, 30, 21), 10) << 1; } while(0)
-
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (SEXT(BITS(i, 7, 7), 1) << 11) \
+  | (SEXT(BITS(i, 30, 25), 6) << 5) | SEXT(BITS(i, 11, 8), 4) << 1; } while(0)
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -48,6 +49,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:		   immJ(); break;
     case TYPE_R: src1R(); src2R(); 	   break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
   }
 }
 
@@ -82,7 +84,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = s->pc+4 ; s->dnpc = s->pc + imm );
   INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(dest) = src1 - src2 );
   INSTPAT("0000000 ????? ????? 000 ????? 01110 11", addw   , R, R(dest) = SEXT(BITS(src1+src2,31,0), 32) );
- 
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if(src1==src2) s->pc += imm );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));    //invalid
   INSTPAT_END();
