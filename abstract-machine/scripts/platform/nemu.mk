a23075bb9a637c1ddbc5984e3ac78dcc047f432f
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-27 17:14:19 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:14:19 up  6:37,  1 user,  load average: 1.62, 0.98, 0.84

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -17,7 +17,7 @@ CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
 .PHONY: $(AM_HOME)/am/src/platform/nemu/trm.c
 
--include $(NEMU_HOME)/configs/am_config 
+-include am_config 
 #auto exec
 
 
