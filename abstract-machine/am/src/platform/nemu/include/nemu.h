1f7a67b968edc48d1d2fee7387b00e59a9cf4be2
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-27 18:33:21 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:21 up  7:56,  1 user,  load average: 0.96, 0.84, 0.78

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -33,7 +33,6 @@
 #define FB_ADDR         (MMIO_BASE   + 0x1000000)
 #define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
 
-#define CONFIG_TARGET_AM y
 
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
