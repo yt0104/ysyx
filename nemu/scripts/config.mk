34ee7f8ffe1a0af7f610df35c5938bf37ed9e7b4
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-28 16:41:52 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:41:52 up  1:07,  1 user,  load average: 0.58, 0.58, 0.56

--- nemu/scripts/config.mk
+++ nemu/scripts/config.mk
@@ -48,7 +48,7 @@ menuconfig: $(MCONF) $(CONF) $(FIXDEP)
 savedefconfig: $(CONF)
 	$(Q)$< $(silent) --$@=configs/defconfig $(Kconfig)
 
-%defconfig: $(CONF) $(FIXDEP)
+defconfig: $(CONF) $(FIXDEP)
 	$(Q)$< $(silent) --defconfig=configs/$@ $(Kconfig)
 	$(Q)$< $(silent) --syncconfig $(Kconfig)
 
