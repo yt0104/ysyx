3435f5a851177a222b17cc9ca36dfd8164845540
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-28 15:48:16 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:48:16 up 13 min,  1 user,  load average: 0.35, 0.63, 0.57

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -23,7 +23,7 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NEMU_HOME)  ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin  -l /configs/am_config
+	$(MAKE) -C $(NEMU_HOME)  ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin  -l $(NEMU_HOME)/configs/config_auto_c
 
 gdb: image
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -18,11 +18,17 @@
 void sdb_mainloop();
 
 void engine_start() {
+
 #ifdef CONFIG_TARGET_AM
   cpu_exec(-1);
 #else
+
+  #ifdef CONFIG_AUTO_C
+    cpu_exec(-1);
+  #endif
+  
   /* Receive commands from user. */
   sdb_mainloop();
-  
+
 #endif
 }
