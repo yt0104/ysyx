9c4b8f8df809f3be86cb25a73859079f6888266b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 10:52:07 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:52:07 up 6 min,  1 user,  load average: 0.47, 0.76, 0.41

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -36,7 +36,7 @@
     } \
   } while (0)
 
-#define panic(format, ...) Assert(0, format, ## __VA_ARGS__)
+#define panic(format, ...)  Assert(0, format, ## __VA_ARGS__)
 
 #define TODO() panic("please implement me")
 
