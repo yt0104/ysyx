ff866270266879c4e9156f0ba5aacef49b7691db
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 11:05:55 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:05:55 up 20 min,  1 user,  load average: 0.80, 0.84, 0.66

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -58,10 +58,8 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  
+  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len)); 
   out_of_bound(addr);
-  puts_iringbuf();
   return 0;
 }
 
@@ -69,5 +67,5 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
-  puts_iringbuf();
+  
 }
