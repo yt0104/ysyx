2f1c3505432e96a12fc6c66b0bdd0206ae3e4e81
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 11:52:28 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:52:28 up 49 min,  1 user,  load average: 0.85, 0.66, 0.52

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -59,7 +59,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) {
-    //IFDEF(CONFIG_MTARCE, printf("read : addr=%d, len=%d\n",addr, len));
+    //IFDEF(CONFIG_MTARCE, printf("read : addr=%x, len=%d\n",addr, len));
     printf("read : addr=%d, len=%d\n",addr, len);
     return pmem_read(addr, len);
     }
