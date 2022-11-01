b68df15ca568d2f2e1add8cee47f1dac54796782
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-01 22:06:11 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:06:11 up  8:00,  1 user,  load average: 0.75, 0.43, 0.27

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -143,19 +143,3 @@ int isa_exec_once(Decode *s) {
 }
 
 
-#define MAX_INST_TO_SAVE 20
-char iringbuf [MAX_INST_TO_SAVE][128];
-int inst_p = 0;
-
-void update_iringbuf(char *s){
-  strcpy(iringbuf[inst_p], s);
-  inst_p = (inst_p+1) % MAX_INST_TO_SAVE;
-
-};
-
-void puts_iringbuf(){
-  printf("%d LAST INSTS is shown:\n",MAX_INST_TO_SAVE);
-  for(int i = inst_p; i < inst_p + MAX_INST_TO_SAVE; i++){
-    if(*iringbuf[i%MAX_INST_TO_SAVE] != '\0') puts(iringbuf[i%MAX_INST_TO_SAVE]);
-  }
-};
