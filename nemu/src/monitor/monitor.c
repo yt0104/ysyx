5b931786fbf54b96502363fba4b0d4e27b9f4f5b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-03 14:52:43 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:52:43 up 48 min,  1 user,  load average: 0.93, 0.99, 0.81

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -100,11 +100,6 @@ static int parse_args(int argc, char *argv[]) {
 
 void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
-  printf("%s\n",argv[0]);
-  printf("%s\n",argv[1]);
-  printf("%s\n",argv[2]);
-  printf("%s\n",argv[3]);
-  printf("%s\n",argv[4]);
 
   /* Parse arguments. */
   parse_args(argc, argv);
