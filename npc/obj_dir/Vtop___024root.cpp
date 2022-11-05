2be4e217b33a1318fce17373952d30bc920e21ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-05 20:53:01 +0800

    > sim RTL ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:53:01 up  5:39,  1 user,  load average: 0.80, 0.57, 0.51

--- npc/obj_dir/Vtop___024root.cpp
+++ npc/obj_dir/Vtop___024root.cpp
@@ -1887,7 +1887,7 @@ VL_INLINE_OPT void Vtop___024root___sequent__TOP__3(Vtop___024root* vlSelf) {
         __Vdly__top__DOT__cpu = ((IData)(vlSelf->exit_flag)
                                   ? 1U : vlSelf->top__DOT__cpu);
         vlSelf->pc = ((0U == vlSelf->top__DOT__cpu)
-                       ? vlSelf->top__DOT__npc : vlSelf->pc);
+                       ? (4ULL + vlSelf->pc) : vlSelf->top__DOT__npc);
     } else {
         __Vdly__top__DOT__cpu = 0U;
         vlSelf->pc = 0x80000000ULL;
