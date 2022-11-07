836d713719294e00a8b6ec6c49a6e60e78e588d7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-07 19:21:42 +0800

    > sim RTL ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:21:42 up 10:07,  1 user,  load average: 0.70, 0.49, 0.35

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -120,8 +120,9 @@ int main(int argc, char *argv[]) {
   top->inst = 0;
   top->rst_n = 1; step_and_dump_wave();
   top->rst_n = 0; step_and_dump_wave();
+  top->inst = ifetch(top->pc, 4); step_and_dump_wave(); // inst 
   top->rst_n = 1; step_and_dump_wave();
-  step_and_dump_wave();   //4s reset
+  step_and_dump_wave();   //5s reset
 
   int main_time = 0;     // 仿真时间戳
   int sim_time = 50;   // 最大仿真时间戳
