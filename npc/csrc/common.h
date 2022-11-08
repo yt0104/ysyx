959db084200e1b7f0b716367b33aa54d4351592b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-08 14:42:14 +0800

    > sim RTL ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:42:14 up 41 min,  1 user,  load average: 0.86, 0.93, 0.86

--- npc/csrc/common.h
+++ npc/csrc/common.h
@@ -1,7 +1,9 @@
-
-#define uint8_t     unsigned char
-#define uint32_t    unsigned int
-#define uint64_t    unsigned long
+#include <iostream>
+#include <assert.h>
+#include <cstdint>
+//#define uint8_t     unsigned char
+//#define uint32_t    unsigned int
+//#define uint64_t    unsigned long
 
 #define CONFIG_MBASE 0x80000000
 #define CONFIG_MSIZE 0x8000000
@@ -10,4 +12,8 @@
 #define PMEM_LEFT  ((uint32_t)CONFIG_MBASE)
 #define PMEM_RIGHT ((uint32_t)CONFIG_MBASE + CONFIG_MSIZE - 1)
 #define RESET_VECTOR (PMEM_LEFT + CONFIG_PC_RESET_OFFSET)
+
+
+uint64_t ifetch(uint32_t addr, int len);
+void load_img(int argc, char *argv[]);
 
---
+++ npc/csrc/memory.cpp
@@ -0,0 +1,53 @@
+#include "common.h"
+
+/********************************************/
+/*memory*/
+
+static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+
+uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+
+void load_img(int argc, char *argv[]) {
+  
+  char *img_file = argv[1];
+
+  if (img_file == NULL) assert(0);
+
+  FILE *fp = fopen(img_file, "rb");
+  if(fp == NULL) assert(0);
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  printf("The image is %s, size = %ld\n", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return;
+}
+
+
+uint64_t host_read(void *addr, int len){
+  switch (len) {
+    case 1: return *(uint8_t  *)addr;
+    case 2: return *(uint16_t *)addr;
+    case 4: return *(uint32_t *)addr;
+    case 8: return *(uint64_t *)addr;
+    default: assert(0);
+  }
+}
+
+
+uint64_t pmem_read(uint32_t addr, int len) {
+  uint64_t ret = host_read(guest_to_host(addr), len);
+  return ret;
+}
+
+uint64_t ifetch(uint32_t addr, int len) {
+  return pmem_read(addr, len);
+}
+
+/*memory end*/
