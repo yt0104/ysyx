56f2b6898950f1842206f030470544d10a2dfad0
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-10 20:39:42 +0800

    > sim RTL ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:39:42 up  4:55,  1 user,  load average: 0.87, 0.79, 0.78

--- nemu/include/cpu/difftest.h
+++ nemu/include/cpu/difftest.h
@@ -36,7 +36,7 @@ static inline void difftest_attach() {}
 #endif
 
 extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
-extern void (*ref_difftest_regcpy)(void *dut, bool direction);
+extern void (*ref_difftest_regcpy)(void *dut_gpr, void *dut_pc, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
 extern void (*ref_difftest_raise_intr)(uint64_t NO);
 
