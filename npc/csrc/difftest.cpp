3273fe43aae91ddd2574f610614107065a189931
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-10 21:35:57 +0800

    > sim RTL ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:35:57 up  5:51,  1 user,  load average: 0.80, 0.52, 0.30

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -57,9 +57,9 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
 
   uint64_t dut[33];
-  //for (size_t i = 0; i < 32; i++) dut[i] = cpu_gpr[i];
-  //dut[32] = top->pc;
-  ref_difftest_regcpy(dut  , DIFFTEST_TO_REF);
+  for (size_t i = 0; i < 32; i++) dut[i] = cpu_gpr[i];
+  dut[32] = top->pc;
+  //ref_difftest_regcpy(dut  , DIFFTEST_TO_REF);
   puts("333");
 }
 
