4be7aa198421f14b4bc5dca14c096c741611498e
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-11 11:55:12 +0800

    > compile NEMU ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:55:12 up  1:18,  1 user,  load average: 0.57, 0.76, 0.70

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -34,12 +34,14 @@ void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
 void difftest_regcpy(void *dut_gpr, void *dut_pc, bool direction) {
   
   word_t *g = (word_t*)dut_gpr; 
-  
+
   if(direction == DIFFTEST_TO_REF){
     for (size_t i = 0; i < 32; i++){
       cpu.gpr[i] = g[i];
     }
     cpu.pc = *(word_t*)dut_pc;
+
+    printf("%ld\n", cpu.pc);
 
   }
 
