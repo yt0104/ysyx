8d830790a8dc8212e486f71c966712891b3968e7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-14 15:32:17 +0800

    > sim RTL ysyx_22050327 杨涛 Linux computerYT 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:32:17 up  5:02,  1 user,  load average: 1.22, 0.75, 0.65

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -66,8 +66,9 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
       wmask = wmask >> 1;
       waddr ++;
     }
+    return;
   }
-  //out_of_bound(waddr);
+  out_of_bound(waddr);
   return;
 }
 
